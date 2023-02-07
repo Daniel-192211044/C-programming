5e69473e18c80d2ab4b7d0882be44455d165ac95
Daniel-192211044 <123558999+Daniel-192211044@users.noreply.github.com>
2023-02-07 10:13:30 +0530

    Update Q1 moderate.cpp

--- Q1 moderate.cpp
+++ Q1 moderate.cpp
@@ -1,28 +1,18 @@
 #include <stdio.h>
 
-int main()
-{
-    int year;
-    printf("Enter a year: ");
-    scanf("%d", &year);
-
-    if (year % 400 == 0) 
-    {
-        printf("%d is a leap year.", year);
-    } 
-    else if (year % 100 == 0) 
-    {
-        printf("%d is not a leap year.", year);
-    } 
-    else if (year % 4 == 0) 
-    {
-        printf("%d is a leap year.", year);
-    } 
-    else 
-    {
-        printf("%d is not a leap year.", year);
-    }
-
-    return 0;
+int main() {
+  int day, month, year;
+  
+  printf("Enter a date (dd mm yyyy): ");
+  scanf("%d %d %d", &day, &month, &year);
+  
+  printf("The date you entered is: %d/%d/%d\n", day, month, year);
+  
+  if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+    printf("%d is a leap year.\n", year);
+  } else {
+    printf("%d is not a leap year.\n", year);
+  }
+  
+  return 0;
 }
-In 
